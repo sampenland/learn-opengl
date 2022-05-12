ad03588291743c2d98a29cac71ab9e501038b268
Sam <sampenland@gmail.com>
2022-05-12 10:02:13 -0400

    Initial commit

---
+++ learn-opengl/src/main.cpp
@@ -0,0 +1,18 @@
+#include <glad/glad.h>
+#include <string>
+#include <iostream>
+
+void Print(const std::string& text)
+{
+	std::cout << text << std::endl;
+}
+
+int main()
+{
+
+	
+
+
+	return 0;
+
+}
